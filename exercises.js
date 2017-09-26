cce5ecc2614c3ca42fc7a7343322dd77f4d8db47
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-26 12:59:27 -0500

    solved problem 2

--- exercises.js
+++ exercises.js
@@ -1,35 +1,54 @@
 'use strict';
 
-function max(numbers) {
+//1
 
+// function max(numbers) {
+
+//   let i = 0;
+//   let maximum = numbers[0];
+
+//   while (i <= numbers.length) {
+//     if (numbers[i] > maximum) {
+//       maximum = numbers[i];
+//     }
+
+//     i++;
+//   }
+
+//   console.log(maximum);
+// }
+
+// max([1, 3, 6, 2, 4, 9, 0]);
+
+// function min(numbers) {
+//   let i = 0;
+//   let minimum = numbers[0];
+
+//   while (i <= numbers.length) {
+//     if (numbers[i] < minimum) {
+//       minimum = numbers[i];
+//     }
+//     i++;
+//   }
+
+//   console.log(minimum);
+// }
+
+// min([1, -8, 3, 11, -20, 16, 3]);
+
+
+//2
+
+
+function average(numbers) {
+  let total = 0;
+  //We're just initializing total.
   let i = 0;
-  let maximum = numbers[0];
-
-  while (i <= numbers.length) {
-    if (numbers[i] > maximum) {
-      maximum = numbers[i];
-    }
-
+  while (i < numbers.length){
+    total += numbers[i];
     i++;
   }
-
-  console.log(maximum);
+  console.log(total/numbers.length);
 }
 
-max([1, 3, 6, 2, 4, 9, 0]);
-
-function min(numbers) {
-  let i = 0;
-  let minimum = numbers[0];
-
-  while (i <= numbers.length) {
-    if (numbers[i] < minimum) {
-      minimum = numbers[i];
-    }
-    i++;
-  }
-
-  console.log(minimum);
-}
-
-min([1, -8, 3, 11, -20, 16, 3]);
+average([1, 2, 3, 4, 5]);
