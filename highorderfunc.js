a493998c877e8a89b7f56e423d66cb971c9faf93
Andy Gaines <andyagaines@gmail.com>
2017-09-26 15:01:09 -0500

    added higher order prob 2

--- highorderfunc.js
+++ highorderfunc.js
@@ -4,7 +4,7 @@
 function repeat(fn,n) {
   for (let i = 0; i <= n; i++) {
     fn();
-}
+  }
 }
 
 function hello() {
@@ -14,7 +14,31 @@ function goodbye(){
   console.log('Goodbye World');
 }
 
-repeat(hello, 5);
-repeat(goodbye, 5);
+// repeat(hello, 5);
+// repeat(goodbye, 5);
 
 //2
+
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray', 'Randy'];
+
+const filteredNames = filter(myNames, function(name) {
+  // This is a "predicate function" - it's a function that only returns a boolean
+  return name[0] === 'R';
+});
+
+
+// TASK -- DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  const newArray = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i])) {
+      newArray.push(arr[i]);
+    }
+  }
+
+  return newArray;
+}
+
+console.log(filteredNames); // => ['Rich', 'Ray']
